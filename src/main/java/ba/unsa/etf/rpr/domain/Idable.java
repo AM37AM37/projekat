bce2f7867acd7de62926f9eb137109b65e235422
AM37AM37 <aridzal2@etf.unsa.ba>
2023-03-20 13:18:28 +0100

    commit spam

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -9,4 +9,4 @@ public interface Idable {
 
 
 }
-//kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+//kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
